ba4e74360a9b6317703b1f43bbe86aab9a361026
alisa <alekseenko_evgeniy@bk.ru>
2026-02-13 21:43:35 +0700

    add .drawio

--- ex1_tusk2/obj/Debug/net10.0/ex2.AssemblyInfo.cs
+++ ex1_tusk2/obj/Debug/net10.0/ex2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ex2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1123e10eafd62e6f79c0624c3f1b9e5fb3d00de")]
 [assembly: System.Reflection.AssemblyProductAttribute("ex2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ex2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
